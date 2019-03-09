eee2154e1e5ad1a8d16cc0c477d4d9d0b3d8d4b3
Cunqi <cunqi.xiao@gmail.com>
2019-03-09 13:52:40 -0800

    Update CXLog

--- CXSync/CXLog.swift
+++ CXSync/CXLog.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
-enum CXLogLevel: Int {
+enum CXLogLevel: Int, Comparable {
+    static func < (lhs: CXLogLevel, rhs: CXLogLevel) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+    
     case error = 0, warn, info, debug, all, off
 
     var tag: String {
@@ -33,24 +37,41 @@ class CXLog {
     static var logLevel: CXLogLevel = .debug
 
     static func E(_ message: String) {
-        guard logLevel.rawValue >= CXLogLevel.error.rawValue else {
+        guard logLevel >= CXLogLevel.error else {
             return
         }
+        log(logLevel.tag, message)
+    }
+    
+    static func W(_ message: String) {
+        guard logLevel >= CXLogLevel.warn else {
+            return
+        }
+        log(logLevel.tag, message)
     }
 
-    static func I(_ message) {
-
+    static func I(_ message: String) {
+        guard logLevel >= CXLogLevel.info else {
+            return
+        }
+        log(logLevel.tag, message)
     }
 
-    static func D(_ message) {
-
+    static func D(_ message: String) {
+        guard logLevel >= CXLogLevel.debug else {
+            return
+        }
+        log(logLevel.tag, message)
     }
 
-    static func A(_ message) {
-
+    static func A(_ message: String) {
+        guard logLevel >= CXLogLevel.all else {
+            return
+        }
+        log(logLevel.tag, message)
     }
-
-    static func E(_ message) {
-
+    
+    private static func log(_ tag: String, _ message: String) {
+        print("\(tag): \(message)")
     }
 }
